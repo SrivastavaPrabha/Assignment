a7145776bd475b051d20bff4b39492a4ebc22665
Prabha Srivastava <srivastavaprabha13@gmail.com>
2023-01-12 16:31:44 +0530

    Volume of cone

---
+++ s.py
@@ -0,0 +1,15 @@
+from flask import Flask,request
+
+app = Flask(__name__)
+
+@app.route("/",methods=['GET'])
+def volume():
+    args=request.args
+    pie = 3.14
+    radius = args.get("radius", default="", type=float)
+    height = args.get("height", default="", type=float)
+    volume = (pie * (radius**2) * height)/3
+    return {"volume": volume}
+
+if __name__ == "__main__":
+   app.run()
